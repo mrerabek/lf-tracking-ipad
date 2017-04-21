a380a096ce2fc01cdd20924e3e669852da2b3315
Tanguy Albrici <tanguy.albrici@epfl.ch>
2017-04-21 16:02:23 +0200

    set up Xcode

---
+++ LFTracking/LFTracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LFTracking
+//
+//  Created by Tanguy Albrici on 21.04.17.
+//  Copyright © 2017 Tanguy Albrici. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
